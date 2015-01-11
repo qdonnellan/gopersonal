b29d4b9da6a3248ea0125eba2271a8d7674330e9
Quentin Donnellan <qdonnellan@gmail.com>
2015-01-11 10:41:25 -0600

    refactoring the routing methods to be a bit more flexible

--- app/app.go
+++ app/app.go
@@ -5,11 +5,23 @@ import (
     "github/go-personal/views"
 )
 
+// route is a wrapper around http.HandleFunc that allows me to pass a string
+// variable to my viewHandler functions
+func route(path string, handler views.ViewHandler, routeName string) {
+    http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+        handler(w, r, routeName)
+   })
+}
+
+func staticRoute(staticPath string) {
+    http.HandleFunc(staticPath, views.StaticFileHandler)
+}
+
 func init() {
-    http.HandleFunc("/", views.FrontPageView)
-    http.HandleFunc("/about", views.ConstructionPageView)
-    http.HandleFunc("/contact", views.ConstructionPageView)
-    http.HandleFunc("/projects", views.ProjectPageView)
-    http.HandleFunc("/essays/", views.EssayPageView)
-    http.HandleFunc("/static/", views.StaticFileViewHandler)
+    route("/", views.BasicPageView, "frontPage")
+    route("/projects", views.BasicPageView, "projectPage")
+    route("/essays", views.BasicPageView, "essayListPage")
+    route("/about", views.BasicPageView, "underConstruction")
+    route("/contact", views.BasicPageView, "underConstruction")
+    staticRoute("/static/")
 }
--- views/views.go
+++ views/views.go
@@ -5,48 +5,32 @@ import (
     "github/go-personal/controllers"
 )
 
-// FrontPageView handles request for the / page.
-func FrontPageView(w http.ResponseWriter, r *http.Request) {
-    controllers.RenderBasicPage(w, "frontPage") 
+// ViewHandler describes what each view handler function should look like
+// such that custom wrapping of http.HandleFunc is predictable (see app.go)
+type ViewHandler func(http.ResponseWriter, *http.Request, string)
+
+func BasicPageView(w http.ResponseWriter, r *http.Request, pageName string) {
+    controllers.RenderBasicPage(w, pageName) 
 }
 
-// ContactPageView handles request for the /contact page. 
-func ContactPageView(w http.ResponseWriter, r *http.Request) {
-    controllers.RenderBasicPage(w, "contactPage")
-}
-
-// AboutPageView handles request for the /about page. 
-func AboutPageView(w http.ResponseWriter, r *http.Request) {
-    controllers.RenderBasicPage(w, "aboutPage")
-}
-
-// ConstructionPageView handles requests to show the construction page
-func ConstructionPageView(w http.ResponseWriter, r *http.Request) {
-    controllers.RenderBasicPage(w, "constructionPage")
-}
-
-// ProjectPageView handles request for the /project page. 
-func ProjectPageView(w http.ResponseWriter, r *http.Request) {
-    controllers.RenderBasicPage(w, "projectPage")
-}
-
-// EssayPageView handles requests matching /essays/ prefix or shows the
-// essay list page if just /essays/
+// EssayPageView calls the RenderEssayPage controller to render an individual
+// essay.
 func EssayPageView(w http.ResponseWriter, r *http.Request) {
     // skip the first characters: /essays/ (length 8)
     essayTitle := r.URL.Path[8:]
-    if len(essayTitle) == 0 {
-        controllers.RenderBasicPage(w, "essayListPage")
-    } else {
-        controllers.RenderEssayPage(w, essayTitle)
-    }
+    controllers.RenderEssayPage(w, essayTitle)
 }
 
-// StaticFileViewHandler handles all /static/* requests. 
-func StaticFileViewHandler(w http.ResponseWriter, r *http.Request) {
+// TutorialPageView calls the RenderEssayPage controller to render an individual
+// tutorial.
+func TutorialPageView(w http.ResponseWriter, r *http.Request) {
+    // skip the first characters: /essays/ (length 8)
+    essayTitle := r.URL.Path[8:]
+    controllers.RenderEssayPage(w, essayTitle)
+}
+
+// StaticFileHandler handles all static file requests. 
+func StaticFileHandler(w http.ResponseWriter, r *http.Request) {
     staticFileName := r.URL.Path[1:]
     http.ServeFile(w, r, staticFileName)
 }
-
-
-
